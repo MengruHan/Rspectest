378765f9fb91d6e1979141af758cff6241991d39
‘MengRu <ru.han@openapply.com>
2020-09-25 15:36:05 +0800

    no message

--- newmodel/db/schema.rb
+++ newmodel/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_070621) do
+ActiveRecord::Schema.define(version: 2020_09_24_091725) do
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
@@ -18,15 +18,18 @@ ActiveRecord::Schema.define(version: 2020_09_03_070621) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "gender"
-    t.integer "mobile"
-    t.string "address"
   end
 
-  create_table "person01s", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
